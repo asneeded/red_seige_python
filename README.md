9759348e218bb1cf23e52bfc9877013885eafbef
asneeded <adamdsneed@gmail.com>
2021-10-26 06:59:31 -0400

    week2 add argparser

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 A serious of python projects that a useful during a Red Team engagement. 
 
 ## Projects
-- [ ] [Week 1](week1/README.md) - IP Geolocation
-- [ ] Week 2 - 
+- [ ] [Week 1](week1) - IP Geolocation
+- [ ] [Week 2](week2) - Web Headers Report
 - [ ] Week 3 - 
 - [ ] Week 4 - 
 - [ ] Week 5 - 
--- week2/web_header.py
+++ week2/web_header.py
@@ -1,1 +1,29 @@
 import requests
+import argparse
+
+app_parser = argparse.ArgumentParser(description="Parse URL headers to check if the Strict-Transport-Security flag is set")
+app_parser.add_argument('--url', '-u', help="url to parse")
+app_parser.add_argument('--file', '-f', help="file location to read urls from")
+
+
+def get_url_headers(url):
+    req = requests.get(url)
+    return req.headers
+
+def check_sts(response_headers):
+    if 'Strict-Transport-Security' in response_headers:
+        return True
+    
+
+def main():
+    url = 'https://www.redsiege.com/'
+    headers = get_url_headers(url)
+    if not check_sts(headers):
+        print(f"{url} is missing Strict-Transport-Security")
+    else:
+        print(f"The Strict-Transport-Security header for {url} is set to {headers['Strict-Transport-Security']}")
+
+    
+
+if __name__ == '__main__':
+    main()
